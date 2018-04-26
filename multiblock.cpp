c5f23e8072da438c306832bcac36a4ceca1be17d
Jakub Trnka <jakub.trnka@chainanalytics.net>
2018-04-26 09:50:56 +0200

    SLIP0039 packets/shares index, threshold, data, checksum serialization/deserialization

--- multiblock.cpp
+++ multiblock.cpp
@@ -23,6 +23,7 @@ std::vector<uint8_t> checksummed16::serialize() const {
 	tmp = htobe16(tmp);
 	output.back() |= tmps[0];
 	output.push_back(tmps[1]);
+	//std::cout << std::hex << "<" << std::setfill('0') << std::setw(2) << (int)hash[0] << std::setfill('0') << std::setw(2) << (int)hash[1] << ">\n";
 
 	tmp = static_cast<uint16_t>(hash[1]);
 	tmp <<= ((8 - (data_bits % 8)) % 8);
@@ -86,7 +87,77 @@ checksummed16::checksummed16(const std::vector<uint8_t> & data, size_t bits) : s
 	data_bits = bits;	
 }
 
+share::share(const std::vector<uint8_t> & p) {
+	if (p.size() < 5) throw "invalid share packet";
+	checksummed16 payload(p, p.size() * 8 - 6);
+	if (!payload.deserialize()) throw "checksum verification failed";
+	uint16_t overflower(0);
+	uint8_t * tmp_ar = reinterpret_cast<uint8_t *>(&overflower);
+	tmp_ar[0] = payload[0];
+	tmp_ar[1] = payload[1];
+	overflower = be16toh(overflower);
+	index = (overflower >> 11) + 1;
+	threshold = ((overflower >> 6) & 31) + 1;
+	for (size_t i = 2; i < payload.size(); ++i) {
+		tmp_ar[0] = payload[i - 1];
+		tmp_ar[1] = payload[i];
+		overflower = be16toh(overflower);
+		overflower <<= 2;
+		overflower = htobe16(overflower);
+		std::cout << std::hex << std::setw(2) << (int) tmp_ar[0]; //prints actual data asdfdfsa78987
+		data.push_back(tmp_ar[0]);
+	}
+	std::cout << std::endl; // asdfdfsa78987
+}
+
+checksummed16 make_share(uint16_t index, uint16_t threshold, const std::vector<uint8_t> & data) {
+	//if (data_bitlen == 0) throw "data payload needs to be greater than 0 bits";
+
+	uint16_t tmp_it = (index - 1) << 11;
+	tmp_it |= ((threshold - 1) & 31)  << 6;
+	tmp_it = htobe16(tmp_it);
+	uint8_t * tmp_array = reinterpret_cast<uint8_t *>(& tmp_it);
+
+	std::vector<uint8_t> wrapper;
+	wrapper.push_back(tmp_array[0]);
+	wrapper.push_back(tmp_array[1]);
+	for (auto i: data) {
+		//uint16_t overflower = htobe16(static_cast<uint16_t>(i) << 6);
+		tmp_it = htobe16(static_cast<uint16_t>(i) << 6);
+		//uint8_t * bytesplit = reinterpret_cast<uint8_t *>(&overflower);
+		wrapper.back() |= tmp_array[0];
+		wrapper.push_back(tmp_array[1]);
+	}
+	return checksummed16(wrapper, data.size() + 10);
+}
+
+void f3() {
+	//uint8_t tmp[] = {0xff,0xcf,0xcf,0xcf,0xdd,0x64,0x40};
+	uint8_t tmp[] = {0xff,0xcf,0xcf,0xcf,0xa1,0x45,0x00};
+	            
+	std::vector<uint8_t> data(std::begin(tmp), std::end(tmp));
+	//for (auto i: data) std::cout << (int) i << ' ';
+	try{
+		share sss(data);
+	} catch (const char *s) {
+		std::cerr << s;
+	}
+}
+
 void f() {
+	std::vector<uint8_t> payload(32,63);
+	//for (auto i: payload) std::cout << std::hex << std::setfill('0') << std::setw(2) <<i;
+	//std::cout << '\n';
+	checksummed16 packet = make_share(32 - 8, 32 - 4, payload);
+	std::vector<uint8_t> packetser(packet.serialize());
+	for (auto i: packetser) std::cout << std::hex << std::setfill('0') << std::setw(2) << (int) i;
+	std::cout << std::endl << "---------------\nRestores:\n";
+	share restored(packetser);
+	for (auto i: restored.data) std::cout << std::hex << std::setfill('0') << std::setw(2) << (int) i;
+	std::cout << std::dec << "\nindes: " << restored.index << "\nthreshold: " << restored.threshold << std::endl;
+}
+
+void f1() {
 	std::vector<uint8_t> vvv;
 	for (int i=0; i<5; ++i) vvv.push_back((uint8_t) 255);
 	checksummed16 cont(vvv, 27);
--- multiblock.h
+++ multiblock.h
@@ -10,14 +10,20 @@ class checksummed16: public std::vector<uint8_t> {
 	public:
 		checksummed16(const std::vector<uint8_t> & data);
 		checksummed16(const std::vector<uint8_t> & data, size_t bits);
-		bool append(std::vector<uint8_t> & data);
-		bool append(std::vector<uint8_t> & data, size_t bits);
 		std::vector<uint8_t> serialize() const; // returns data with 16bit checksum appended
 		bool deserialize(); // checks if last 16 bits is valid checksum and removes it from data
 		size_t getLength() const { return data_bits; }
 };
 
+struct share {
+	public:
+		uint16_t index, threshold;
+		std::vector<uint8_t> data;
+		size_t data_bitlen;
+		share(const std::vector<uint8_t> & p);
+};
 
+checksummed16 make_share(uint16_t index, uint16_t threshold, const std::vector<uint8_t> & data, size_t data_bitlen);
 
 void f();
 
