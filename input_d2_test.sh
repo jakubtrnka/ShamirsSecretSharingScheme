ec09366c0223ec36e6c30acde19c02b7778754c8
Jakub Trnka <jakub.trnka@chainanalytics.net>
2018-06-12 19:10:27 +0200

    Deprecated tests removed

--- input_d2_test.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-./shamir -d -t 3 -n 10 << EOF
-relax switch tackle bird flower ahead bike essay topic air tunnel december issue transfer debate
-EOF
--- input_d_test.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-./shamir -d -t 3 -n 5 << EOF
-file screen pave gloom ivory glimpse post circle harvest answer vehicle holiday
-EOF
-
